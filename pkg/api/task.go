4c181b3a1caa40ca5dd1cbff236ea9c8484b6b8f
Diana <diana_yusupovaa@mail.ru>
2025-12-31 22:29:32 +0300

    added GetTask and UpdateTask

--- pkg/api/task.go
+++ pkg/api/task.go
@@ -1,12 +1,62 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
+	"time"
+
+	"go1f/pkg/db"
 )
 
-func taskHandler(w http.ResponseWriter, r *http.Request) {
+func TaskHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
-		addTaskHandler(w, r)
+		AddTaskHandler(w, r)
+
+	case http.MethodGet:
+		id := r.URL.Query().Get("id")
+		if id == "" {
+			WriteJson(w, map[string]string{"error": "Не указан идентификатор задачи"})
+			return
+		}
+
+		task, err := db.GetTask(id)
+		if err != nil {
+			WriteJson(w, map[string]string{"error": "Задача не найдена"})
+			return
+		}
+		WriteJson(w, task)
+
+	case http.MethodPut:
+		var task db.Task
+
+		err := json.NewDecoder(r.Body).Decode(&task)
+		if err != nil {
+			WriteJson(w, map[string]string{"error": "Ошибка чтения JSON"})
+			return
+		}
+
+		if task.ID == "" {
+			WriteJson(w, map[string]string{"error": "Не указан идентификатор задачи"})
+			return
+		}
+		if task.Title == "" {
+			WriteJson(w, map[string]string{"error": "Не указан заголовок задачи"})
+			return
+		}
+		if _, err := time.Parse("20060102", task.Date); err != nil {
+			WriteJson(w, map[string]string{"error": "Неверный формат даты"})
+			return
+		}
+
+		if err := db.UpdateTask(&task); err != nil {
+			WriteJson(w, map[string]string{"error": err.Error()})
+			return
+		}
+
+		WriteJson(w, map[string]interface{}{})
+
+	default:
+		WriteJson(w, map[string]string{"error": "Метод не поддерживается"})
 	}
 }
